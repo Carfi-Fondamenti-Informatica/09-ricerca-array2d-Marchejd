4dda345b9de629608473db6fbc76efe266e33b4f
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-13 16:34:10 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,7 +2,7 @@
 int controllo (char ar[][20], char test[][20])
 {
     int cont = 0;
-    for (int i = 0; i<2; i++)
+    for (int i = 0; i<10; i++)
     {
         cont++;
         if (ar[i][20] == test[1][20])
