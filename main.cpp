fcf57712ae715534b0061fd334309481f26f02ae
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-13 16:32:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+char l[2][20]; //10 nomi con lunghezza massima 20 caratteri
+    for (int i=0; i<2; i++)
+    {
+                cin >> l[i];
+    }
+    char ins[1][20];
+    cout<<"inserire il nome da cercare : "<<endl;
+    cin >> ins[1][20];
+    controllo(l,ins);
   return 0;
 }
