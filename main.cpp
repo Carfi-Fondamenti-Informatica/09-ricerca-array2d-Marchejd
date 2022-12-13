9629811cda1999b06e2962757f326fff2d76c7f0
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-13 16:33:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "lib.h"
 using namespace std;
 int main(){
-char l[2][20]; //10 nomi con lunghezza massima 20 caratteri
-    for (int i=0; i<2; i++)
+char l[10][20]; //10 nomi con lunghezza massima 20 caratteri
+    for (int i=0; i<10; i++)
     {
                 cin >> l[i];
     }
