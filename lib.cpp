cda59d7d8c808affe3b56c42f5ec6850ae1f71b4
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-13 16:30:26 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "lib.h"
+int controllo (char ar[][20], char test[][20])
+{
+    int cont = 0;
+    for (int i = 0; i<2; i++)
+    {
+        cont++;
+        if (ar[i][20] == test[1][20])
+        {
+            cout<<"la posizione è : "<<cont<<endl;
+        }
+    }
+}
