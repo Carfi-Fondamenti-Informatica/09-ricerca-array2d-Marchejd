d5294661e3331a23411163dad092842932dbb706
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-16 16:28:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include "lib.h"
-using namespace std;
-int main(){
-char l[10][20]; //10 nomi con lunghezza massima 20 caratteri
-    for (int i=0; i<10; i++)
-    {
-                cin >> l[i];
+    using namespace std;
+
+  int main(){
+        char nomi [10][20] ;
+        char proprio[20] ;
+        int p=1;
+        for(int i=0; i<10; i++){
+            for(int k=0; k<20; k++){
+                nomi[i][k]=' ';
+                proprio[k]=' ';
+            }
+        }
+        for(int i=0; i<10; i++){
+            cin >> nomi[i];
+        }
+        cin>>proprio;
+        if(controllo(proprio, nomi, p)){
+            cout << p << endl;
+        }else{
+            cout << "non presente" << endl;
+        }
+        return 0;
     }
-    char ins[1][20];
-    cout<<"inserire il nome da cercare : "<<endl;
-    cin >> ins[1][20];
-    controllo(l,ins);
-  return 0;
-}
